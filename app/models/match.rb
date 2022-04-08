fc40beb07321112348fc2e6a38c44d35f022f3b8
Matt Sroufe <mattsroufe@gmail.com>
2022-04-08 22:18:38 +1200

    update matches show page

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,5 @@ class Match < ApplicationRecord
   belongs_to :course
   has_many :match_teams, dependent: :destroy
   has_many :teams, through: :match_teams
+  has_many :match_hole_player_scores
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
   belongs_to :team
+  has_many :match_hole_player_scores
 end
