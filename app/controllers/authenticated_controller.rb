8bf77fbb79fbbe2f3c927f3aaab09a373c0632d6
Matt Sroufe <mattsroufe@gmail.com>
2022-04-23 07:07:40 +1200

    rescue a couple of types of errors

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -8,7 +8,7 @@ class AuthenticatedController < ApplicationController
       ActiveRecord::Base.connection.execute("SET LOCAL ROLE #{session[:username]}")
       yield
     end
-  rescue ActiveRecord::StatementInvalid => exception
+  rescue *[PG::InsufficientPrivilege, ActiveRecord::StatementInvalid] => exception
     render body: exception, status: :forbidden
   end
 end
