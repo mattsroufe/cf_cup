d4310d600b398c39647b23747b648aea37a2809e
Matt Sroufe <mattsroufe@gmail.com>
2022-04-24 14:06:57 +1200

    Revert "rescue a couple of types of errors"
    This reverts commit 8bf77fbb79fbbe2f3c927f3aaab09a373c0632d6.

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -8,7 +8,7 @@ class AuthenticatedController < ApplicationController
       ActiveRecord::Base.connection.execute("SET LOCAL ROLE #{session[:username]}")
       yield
     end
-  rescue *[PG::InsufficientPrivilege, ActiveRecord::StatementInvalid] => exception
+  rescue ActiveRecord::StatementInvalid => exception
     render body: exception, status: :forbidden
   end
 end
