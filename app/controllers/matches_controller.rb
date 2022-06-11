cf8f0f39f13eef2c2f113fe493742387b41ddc70
Matt Sroufe <mattsroufe@gmail.com>
2022-06-12 06:50:45 +1200

    add includes

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,7 +3,7 @@ class MatchesController < ApplicationController
 
   # GET /matches or /matches.json
   def index
-    @matches = Match.all
+    @matches = Match.all.includes(:course, teams: :players)
   end
 
   # GET /matches/1 or /matches/1.json
